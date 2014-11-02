919eb1ccdb8a8a6a7210a736fd114d65e3f49fbe
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-11-02 23:15:51 +0800

    Configured nav bar content

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # get 'home' => 'pages#home'
   get 'about' => 'pages#about' # creates about_path
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
