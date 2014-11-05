2d7e3148602e620afbed68bf047bbd0b38997b59
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-11-05 23:49:51 +0800

    Added new pages and updated pagecontroller for team, services and contact page'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,13 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def team
+  end
+
+  def services
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   # get 'home' => 'pages#home'
   get 'about' => 'pages#about' # creates about_path
+  get 'services' => 'pages#services' # creates services_path
+  get 'team' => 'pages#team' # creates team_path
+  get 'contact' => 'pages#contact' # creates contact_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
